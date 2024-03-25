3701cdf3d5dcf60130fef4433879dc016b858ec2
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-03-25 13:33:22 +0300

    Update compile and target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.yusufmendes.jetpackcomposedesignwork2"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.yusufmendes.jetpackcomposedesignwork2"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
