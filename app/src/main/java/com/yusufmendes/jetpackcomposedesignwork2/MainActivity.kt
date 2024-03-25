cab98b293fd1727c7defa68122522a4a367c617c
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-03-25 13:37:21 +0300

    Add mutable state method

--- app/src/main/java/com/yusufmendes/jetpackcomposedesignwork2/MainActivity.kt
+++ app/src/main/java/com/yusufmendes/jetpackcomposedesignwork2/MainActivity.kt
@@ -3,11 +3,17 @@ package com.yusufmendes.jetpackcomposedesignwork2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.yusufmendes.jetpackcomposedesignwork2.ui.theme.JetpackComposeDesignWork2Theme
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Screen()
                 }
             }
         }
@@ -30,17 +36,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Screen() {
+
+    val number = remember { mutableStateOf(10) }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Number : ${number.value}")
+
+        Button(onClick = { number.value = number.value + 2 }) {
+            Text(text = "Click")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeDesignWork2Theme {
-        Greeting("Android")
+        Screen()
     }
 }
